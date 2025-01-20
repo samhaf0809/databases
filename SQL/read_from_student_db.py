a38b19a257030a46bb5369b1499fcd7912f132f9
samhaf0809 <samhaf0809@highgateschool.org.uk>
2025-01-20 15:06:24 +0000

    inital commit of students database

---
+++ SQL/read_from_student_db.py
@@ -0,0 +1,15 @@
+import sqlite3
+conn = sqlite3.connect('students.sqlite')
+cursor = conn.cursor()
+
+select_students = """
+SELECT id,firstname,lastname
+FROM students
+WHERE age >= 15;
+"""
+
+cursor.execute(select_students)
+first_students = cursor.fetchone()
+more_students = cursor.fetchmany(10)
+other_students = cursor.fetchall()
+conn.close()
