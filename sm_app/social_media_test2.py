51c6329e772bc454fb4b9ed0ea74406408827a04
samhaf0809 <samhaf0809@highgateschool.org.uk>
2025-01-23 09:08:15 +0000

    social media app 2

--- sm_app/social_media_test2.py
+++ sm_app/social_media_test2.py
@@ -1,9 +1,7 @@
 import sqlite3
 
-conn = sqlite3.connect('social_media.sqlite')
-cursor = conn.cursor()
 
-users_table = """
+create_users_table = """
 CREATE TABLE IF NOT EXISTS users (
   id INTEGER PRIMARY KEY AUTOINCREMENT,
   name TEXT NOT NULL,
@@ -12,7 +10,7 @@ CREATE TABLE IF NOT EXISTS users (
   nationality TEXT
 );
 """
-posts_table = """
+create_posts_table = """
 CREATE TABLE IF NOT EXISTS posts(
   id INTEGER PRIMARY KEY AUTOINCREMENT, 
   title TEXT NOT NULL, 
@@ -21,87 +19,32 @@ CREATE TABLE IF NOT EXISTS posts(
   FOREIGN KEY (user_id) REFERENCES users (id)
 );
 """
-comments_table = """
+create_comments_table = """
 CREATE TABLE IF NOT EXISTS comments (
   id INTEGER PRIMARY KEY AUTOINCREMENT, 
   text TEXT NOT NULL, 
   user_id INTEGER NOT NULL, 
   post_id INTEGER NOT NULL, 
-  FOREIGN KEY (user_id) REFERENCES users (id) FOREIGN KEY (post_id) REFERENCES posts (id)
+  FOREIGN KEY (user_id) REFERENCES users (id),
+  FOREIGN KEY (post_id) REFERENCES posts (id)
 );
 """
-likes_table = """
+create_likes_table = """
 CREATE TABLE IF NOT EXISTS likes (
   id INTEGER PRIMARY KEY AUTOINCREMENT, 
   user_id INTEGER NOT NULL, 
   post_id integer NOT NULL, 
-  FOREIGN KEY (user_id) REFERENCES users (id) FOREIGN KEY (post_id) REFERENCES posts (id)
+  FOREIGN KEY (user_id) REFERENCES users (id),
+  FOREIGN KEY (post_id) REFERENCES posts (id)
 );
 """
-cursor.execute(users_table)
-cursor.execute(posts_table)
-cursor.execute(comments_table)
-cursor.execute(likes_table)
 
+with sqlite3.connect("sm_app.sqlite") as conn:
+    cursor = conn.cursor()
+    cursor.execute(create_users_table)
+    cursor.execute(create_posts_table)
+    cursor.execute(create_comments_table)
+    cursor.execute(create_likes_table)
+    conn.commit()
 
-create_users = """
-INSERT INTO
-  users (name, age, gender, nationality)
-VALUES
-  ('James', 25, 'male', 'USA'),
-  ('Leila', 32, 'female', 'France'),
-  ('Brigitte', 35, 'female', 'England'),
-  ('Mike', 40, 'male', 'Denmark'),
-  ('Elizabeth', 21, 'female', 'Canada');
-"""
-cursor.execute(create_users)
-
-
-create_posts = """
-INSERT INTO
-  posts (title, description, user_id)
-VALUES
-  ("Happy", "I am feeling very happy today", 1),
-  ("Hot Weather", "The weather is very hot today", 2),
-  ("Help", "I need some help with my work", 2),
-  ("Great News", "I am getting married", 1),
-  ("Interesting Game", "It was a fantastic game of tennis", 5),
-  ("Party", "Anyone up for a late-night party today?", 3);
-"""
-cursor.execute(create_posts)
-
-
-create_comments = """
-INSERT INTO
-  comments (text, user_id, post_id)
-VALUES
-  ('Count me in', 1, 6),
-  ('What sort of help?', 5, 3),
-  ('Congrats buddy', 2, 4),
-  ('I was rooting for Nadal though', 4, 5),
-  ('Help with your thesis?', 2, 3),
-  ('Many congratulations', 5, 4);
-"""
-cursor.execute(create_comments)
-
-
-create_likes = """
-INSERT INTO
-  likes (user_id, post_id)
-VALUES
-  (1, 6),
-  (2, 3),
-  (1, 5),
-  (5, 4),
-  (2, 4),
-  (4, 2),
-  (3, 6);
-"""
-cursor.execute(create_likes)
-
-
-
-
-
-conn.commit()
 conn.close()
---
+++ sm_app/write_to_sm.py
@@ -0,0 +1,59 @@
+import sqlite3
+
+insert_users = """
+INSERT INTO
+    users (name, age, gender, nationality)
+VALUES
+  ('James', 25, 'male', 'USA'),
+  ('Leila', 32, 'female', 'France'),
+  ('Brigitte', 35, 'female', 'England'),
+  ('Mike', 40, 'male', 'Denmark'),
+  ('Elizabeth', 21, 'female', 'Canada');
+"""
+
+insert_posts = """
+INSERT INTO
+  posts (title, description, user_id)
+VALUES
+  ("Happy", "I am feeling very happy today", 1),
+  ("Hot Weather", "The weather is very hot today", 2),
+  ("Help", "I need some help with my work", 2),
+  ("Great News", "I am getting married", 1),
+  ("Interesting Game", "It was a fantastic game of tennis", 5),
+  ("Party", "Anyone up for a late-night party today?", 3);
+"""
+
+insert_comments = """
+INSERT INTO
+  comments (text, user_id, post_id)
+VALUES
+  ('Count me in', 1, 6),
+  ('What sort of help?', 5, 3),
+  ('Congrats buddy', 2, 4),
+  ('I was rooting for Nadal though', 4, 5),
+  ('Help with your thesis?', 2, 3),
+  ('Many congratulations', 5, 4);
+"""
+
+insert_likes = """
+INSERT INTO
+  likes (user_id, post_id)
+VALUES
+  (1, 6),
+  (2, 3),
+  (1, 5),
+  (5, 4),
+  (2, 4),
+  (4, 2),
+  (3, 6);
+"""
+
+with sqlite3.connect("sm_app.sqlite") as conn:
+    cursor = conn.cursor()
+    cursor.execute(insert_users)
+    cursor.execute(insert_posts)
+    cursor.execute(insert_comments)
+    cursor.execute(insert_likes)
+    conn.commit()
+
+conn.close()
